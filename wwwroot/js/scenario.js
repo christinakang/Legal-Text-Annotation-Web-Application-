275e80bc089f5ad657a4958150ec80e06d236889
Rhodian Lu <rhodianlujinghong@hotmail.com>
2022-10-17 19:48:16 +0800

    - Add functionality to select scenarios from the database in the scenario UI - Disable multi select scripts causing errors in scenario page

--- wwwroot/js/scenario.js
+++ wwwroot/js/scenario.js
@@ -1,24 +1,54 @@
 const legalConceptUri = 'api/legalconcepts';
 const scenariosUri = 'api/scenarios';
 
+var scenarioList = null;
+
 // function that runs all necessary initial retrieval.
 function init() {
     // Get the first id scenerio from db.
-    getScenario(1);
+    getScenarioList();
+}
+
+// The the list of scenarios available in the database.
+function getScenarioList() {
+    // call db to get all scenarios.
+    fetch(scenariosUri)
+        .then(response => response.json())
+        .then(data => {
+            scenarioList = data;
+            var scenarioSelect = document.getElementById("scenarioSelect");
+
+            $.each(data, function (index, value) {
+                scenarioSelect.innerHTML +=
+                    ('<option value="' + value.id + '">' + value.id + '</option>\n');
+            });
+        })
+        .catch(error => console.error('Unable to get scenarios', error));
 }
 
 // get scenario based on id.
-function getScenario(id) {
-    fetch(`${scenariosUri}/${id}`)
-        .then(response => response.json())
-        .then(data => changeScenarioText(data))
-        .catch(error => console.error('Unable to get scenario ${id}', error));
+function getScenario() {
+    const scenarioSelect = document.getElementById('scenarioSelect');
+
+    let selectedId = scenarioSelect.value;
+
+    if (selectedId == 0) {
+        var element = document.getElementById("ScenarioText");
+        element.innerHTML = null;
+        return;
+    }
+
+    $.each(scenarioList, function (index, value) {
+        if (value.id == selectedId) {
+            changeScenarioText(value);
+        }
+    });
 }
 
 // Change the scenario text based on retrieved scenario text from db.
-function changeScenarioText(data) {
-    var element = document.getElementById("ScenarioText");
-    element.innerHTML = data.text;
+function changeScenarioText(scenario) {
+    var scenarioText = document.getElementById("ScenarioText");
+    scenarioText.innerHTML = scenario.text;
 }
 
 // Default add scenario logic.
@@ -44,15 +74,6 @@ function addScenario() {
             addNameTextbox.value = '';
         })
         .catch(error => console.error('Unable to add item.', error));
-}
-
-// Default delete scneario logic.
-function deleteItem(id) {
-    fetch(`${uri}/${id}`, {
-        method: 'DELETE'
-    })
-        .then(() => getItems())
-        .catch(error => console.error('Unable to delete item.', error));
 }
 
 // function to get selected text.
