91b8ad2c0c58729858eee2d989619de9f72e3868
Xiaoxi Kang <Xiaoxi.Kang@monash.edu>
2022-10-21 15:53:25 +0800

    Update the index page. Current user will be captured.

--- wwwroot/js/sb-admin-2.js
+++ wwwroot/js/sb-admin-2.js
@@ -61,11 +61,13 @@ function validate() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
     if (username == "admin" && password == "admin") {
-        alert("Login success fully");
         localStorage.setItem('username', username);
         window.location = "index.html"; // Redirecting to other page.
         return false;
+    } else {
+        alert("wrong username or password");
     }
+    
 }
 
 
