28514f441c93d0619df9a308095f4709091f5fc1
Rhodian Lu <rhodianlujinghong@hotmail.com>
2022-10-29 19:48:10 +0800

    - Add creation of buttons for highlighted annotations - Add initial logic for creation of relation buttons - Tidy up the scenario page by adding spacing and guide information for annotation and relation buttons - Remove unused files references in application proj

--- wwwroot/js/scenario.js
+++ wwwroot/js/scenario.js
@@ -7,6 +7,7 @@ var scenarioList = null;
 function init() {
     // Get the first id scenerio from db.
     getScenarioList();
+    createRelationButtons();
 }
 
 // The the list of scenarios available in the database.
@@ -74,4 +75,51 @@ function addScenario() {
             addNameTextbox.value = '';
         })
         .catch(error => console.error('Unable to add item.', error));
+}
+
+function createRelationButtons() {
+    // TODO: Create and get relation information from a relation table?
+
+    // Id, value array. To be replaced with actual table data.
+    const relationData = [
+        { name: 'IfElse', text: 'If ..... Else ....' }];
+
+    // Get relation btn div id.
+    var relationBtnDiv = document.getElementById('relationBtns');
+
+    $.each(relationData, function (index, value) {
+        // Create button element.
+        var button = document.createElement('button');
+
+        button.type = 'button';
+
+        // Id will be the relationName + 'Btn'.
+        // Ex.Selected IfElse => The button id is 'IfElseBtn'.
+        // It is case senstive and also will take in spaces.
+        button.id = value.name + 'Btn';
+
+        // Add text to be displayed on button.
+        button.innerHTML = value.name;
+
+        // Add onClick event to add the selected relation to analysis text area at the cursor.
+        button.addEventListener('click', function (event) {
+            // Get current position of cursor at analysis text area.
+            // If no cursor, it will append at the end of the textArea's text.
+            var analysisTxtBox = document.getElementById('analysisTextArea');
+            let curPos = analysisTxtBox.selectionStart;
+
+            // Get current text in analysisTextArea.
+            var analysisTextAreaValue = $('#analysisTextArea').val();
+
+            // Insert text at cursor position.
+            $('#analysisTextArea').val(
+                analysisTextAreaValue.slice(0, curPos) + value.text + analysisTextAreaValue.slice(curPos));
+
+            // Restore cursor position to end of analysis text area after clicking on button.
+            analysisTextArea.focus();
+        });
+
+        // Append the  button to annotation div.
+        relationBtnDiv.appendChild(button);
+    });
 }
